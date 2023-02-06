8897ed50a7453449311e61755571988fe335146e
Collin Sakuma <collinmsakuma@gmail.com>
2023-02-05 17:48:33 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordArray = turnString(word)
+  const wordArrayRev = [...wordArray].reverse();
+  for (let i = 0; i < wordArray.length; i++) {
+    if(wordArray[i] === wordArrayRev[i]) {
+      return true
+    } else {
+      return false
+    }
+  }  
 }
+
+// turn the word variable in to an array of letters //
+function turnString(word) {
+  return word.split('')
+}
+
+
+
 
 /* 
   Add your pseudocode here
+  need to turn word variable into an array so the order can be
+  flipped and compared against one another. 
 */
 
 /*
@@ -20,6 +39,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome('Collin'));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
 }
 
 module.exports = isPalindrome;
